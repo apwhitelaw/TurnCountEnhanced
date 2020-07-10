3226958e1fe00920c35ed3fe6b46d1e858c7f357
Austin Whitelaw <34988330+apwhitelaw@users.noreply.github.com>
2020-07-10 18:04:59 -0400

    Added new main class to fix error

---
+++ src/main/java/turncount/App.java
@@ -0,0 +1,12 @@
+package turncount;
+
+// MAVEN / JAVAFX JAR ISSUE
+// FIX BY CREATING SEPARATE MAIN CLASS THAT DOESN'T EXTEND APPLICATION
+
+public class App {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
